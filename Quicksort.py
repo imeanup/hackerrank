3e3c4e3c040f5d471543c60789fbad2e32710d61
anupam <tah.anupam@gmail.com>
2022-10-03 23:00:09 +0530

    Update Quicksort.py

--- Quicksort.py
+++ Quicksort.py
@@ -19,7 +19,7 @@ def sort(A, lo, hi):
 def partition(A,lo, hi):
     pivot = A[hi]
     i = lo
-    for j in range(lo, hi - 1):
+    for j in range(lo, hi):
         if A[j] <= pivot:
             A[i], A[j] = A[j], A[i]
             i += 1
