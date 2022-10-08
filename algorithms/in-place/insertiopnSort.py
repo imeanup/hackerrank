646bef6e4a0353f9bc841ed50ee3d8aa876d55d3
anupam <tah.anupam@gmail.com>
2022-10-08 09:45:28 +0530

    Create insertiopnSort.py

---
+++ algorithms/in-place/insertiopnSort.py
@@ -0,0 +1,17 @@
+from random import shuffle
+
+''' Insertion sort can require Ω(n^2) comparisons and Ω(n2) swaps in the worst case. '''
+
+def insertion_sort(A):                    # Insertion sort array A
+    for i in range(1, len(A)):            # O(n) loop over array
+        j = i                             # O(1) initialize pointer
+        while j > 0 and A[j] < A[j -1]:   # O(i) loop over prefix
+            A[j-1],A[j] = A[j],A[j-1]     # O(1) swap
+            j = j-1                       # O(1) decrement j
+
+
+if __name__ == "__main__":
+    A = [i for i in range(1, 101)]
+    shuffle(A)
+    insertion_sort(A)
+    print(A)
