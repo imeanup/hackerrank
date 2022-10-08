242850aab2a09b90afaa5be34cfba04b506f58a6
anupam <tah.anupam@gmail.com>
2022-10-08 10:17:51 +0530

    Create mergeSort.py

---
+++ algorithms/not-in-place/mergeSort.py
@@ -0,0 +1,26 @@
+import random
+
+def merge_sort(A, a = 0, b = None):     # Sort sub-array A[a:b]
+    if b is None:                       # O(1) initialize
+        b = len(A)                      # O(1)
+    if 1 < b - a:                       # O(1) size k = b - a
+        c = (a + b + 1) // 2            # O(1) compute center
+        merge_sort(A, a, c)             # T(k/2) recursively sort left
+        merge_sort(A, c, b)             # T(k/2) recursively sort right
+        L, R = A[a:c], A[c:b]           # O(k) copy
+        i, j = 0, 0                     # O(1) initialize pointers
+        while a < b:                    # O(n)
+            if (j >= len(R)) or (i < len(L) and L[i] < R[j]):   # O(1) check side
+                A[a] = L[i]                                     # O(1) merge from left
+                i = i + 1                                       # O(1) decrement left pointer
+            else:
+                A[a] = R[j]                                     # O(1) merge from right
+                j = j + 1                                       # O(1) decrement right pointer
+                a = a + 1                                       # O(1) decrement merge pointer
+
+
+if __name__ == "__main__":
+    A = [i for i in range(1, 101)]
+    random.shuffle(A)
+    merge_sort(A)
+    print(A)
