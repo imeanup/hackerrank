345805ffe641adf5f668c0f7d352cb4f47a4d2cd
anupam <tah.anupam@gmail.com>
2022-10-12 10:48:38 +0530

    Update insertiopnSort.py

--- algorithms/in-place/insertiopnSort.py
+++ algorithms/in-place/insertiopnSort.py
@@ -7,7 +7,7 @@ def insertion_sort(A):                    # Insertion sort array A
         j = i                             # O(1) initialize pointer
         while j > 0 and A[j] < A[j -1]:   # O(i) loop over prefix
             A[j-1],A[j] = A[j],A[j-1]     # O(1) swap
-            j = j-1                       # O(1) decrement j
+            j -= 1                       # O(1) decrement j
 
 
 if __name__ == "__main__":
