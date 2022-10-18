b9d39fb47274119aa8f4ffd25744ec7b3fcccf83
anupam <tah.anupam@gmail.com>
2022-10-18 20:03:33 +0530

    Create CompresstheString!.py

---
+++ CompresstheString!.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from itertools import groupby
+l= []
+s = input()
+for res, k in (groupby(s)):
+    count = 0
+    for i in k:
+        if i == res:
+            count += 1
+    l.append((count,int(res)))
+    
+print(" ".join(map(str, l)))
