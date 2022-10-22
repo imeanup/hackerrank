70fb489cfe116ee30bf51502fd140311d4c66821
anupam <tah.anupam@gmail.com>
2022-10-22 23:15:01 +0530

    "formatted"

--- algorithms/Binary_Tree/G. has_path_sum.py
+++ algorithms/Binary_Tree/G. has_path_sum.py
@@ -1,11 +1,13 @@
 # Find a root to leaf path with specified sum
 
-def has_path_sum(tree, remaining_weight):       # Time: O(n);Space: O(h)
-  if not tree:
-    return False
-  if not tree.left and not tree.right:    # Leaf.
-    return remaining_weight == tree.data
-  
-  # Non-leaf.
-  return (has_path_sum(tree.left, remaining_weight - tree.data)
-          or has_path_sum(tree.right, remaining_weight - tree.data))
+
+def has_path_sum(tree, remaining_weight):  # Time: O(n);Space: O(h)
+    if not tree:
+        return False
+    if not tree.left and not tree.right:  # Leaf.
+        return remaining_weight == tree.data
+
+    # Non-leaf.
+    return has_path_sum(tree.left, remaining_weight - tree.data) or has_path_sum(
+        tree.right, remaining_weight - tree.data
+    )
