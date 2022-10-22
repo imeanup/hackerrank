e473024aa862a476aa12cc51514ee27b4e540beb
anupam <tah.anupam@gmail.com>
2022-10-22 23:21:29 +0530

    Tree_Traversal

--- algorithms/Binary_Tree/A. tree_traversal.py
+++ algorithms/Binary_Tree/A. tree_traversal.py
@@ -8,3 +8,4 @@ def tree_traversal(root):  # Time: O(n); Space: O(h); h-> height; min(h) = log n
         tree_traversal(root.right)
         # Post-order: Processes the root after the traversals of left and right chiLd.
         print("Postorder: %d" % root.data)
+
