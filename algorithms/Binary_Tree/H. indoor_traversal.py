3e173a582151980ad1b8413c9d9d85d4a6abe6aa
anupam <tah.anupam@gmail.com>
2022-10-22 21:51:26 +0530

    Create H. indoor_traversal.py

---
+++ algorithms/Binary_Tree/H. indoor_traversal.py
@@ -0,0 +1,19 @@
+# Implement an inorder traversal without recursion
+
+def inoder_traversal(tree):     # Time: O(n); space: O(h)
+  s, result = [], []
+  
+  while s or tree:
+    if tree:
+      s.append(tree)
+      # Going left.
+      tree = tree.left
+      
+    else:
+      # Going up.
+      tree = s.pop()
+      result.append(tree.data)
+      # Going right.
+      tree = tree.right
+      
+  return result
