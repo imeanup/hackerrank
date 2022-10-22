80a3592b3f7a94609299d4115cffebed6a0cac1a
anupam <tah.anupam@gmail.com>
2022-10-22 23:15:31 +0530

    "formatted"

--- algorithms/Binary_Tree/H. indoor_traversal.py
+++ algorithms/Binary_Tree/H. indoor_traversal.py
@@ -1,19 +1,20 @@
 # Implement an inorder traversal without recursion
 
-def inoder_traversal(tree):     # Time: O(n); space: O(h)
-  s, result = [], []
-  
-  while s or tree:
-    if tree:
-      s.append(tree)
-      # Going left.
-      tree = tree.left
-      
-    else:
-      # Going up.
-      tree = s.pop()
-      result.append(tree.data)
-      # Going right.
-      tree = tree.right
-      
-  return result
+
+def inoder_traversal(tree):  # Time: O(n); space: O(h)
+    s, result = [], []
+
+    while s or tree:
+        if tree:
+            s.append(tree)
+            # Going left.
+            tree = tree.left
+
+        else:
+            # Going up.
+            tree = s.pop()
+            result.append(tree.data)
+            # Going right.
+            tree = tree.right
+
+    return result
