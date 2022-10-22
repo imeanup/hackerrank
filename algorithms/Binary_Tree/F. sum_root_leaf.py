4c9c6e35b3624560def54f3017fff4f9640a0527
anupam <tah.anupam@gmail.com>
2022-10-22 21:41:38 +0530

    Create F. sum_root_leaf.py

---
+++ algorithms/Binary_Tree/F. sum_root_leaf.py
@@ -0,0 +1,12 @@
+# Sum the root to leaf path in a Binary Tree
+
+def sum_root_to_leaf(tree, partial_path_sum=0):     # Space: O(h);Time: O(n)
+  if not tree:
+    return 0
+  
+  partial_path_sum = partial_path_sum * 2 + tree.data
+  if not tree.left and not tree.right: # Leaf.
+    return partial_path_sum
+  # Non-leaf.
+  return (sum_root_to_left(tree.left, partial_path_sum) + sum_root_to_leaf(
+    tree.right, partial_path_sum))
