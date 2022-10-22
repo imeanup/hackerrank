8451f7cad729e34c03aa665dba55941c18413d21
anupam <tah.anupam@gmail.com>
2022-10-22 20:59:33 +0530

    Create tree_traversal.py

---
+++ algorithms/Binary_Tree/tree_traversal.py
@@ -0,0 +1,23 @@
+class BinaryTreeNode:
+  def __init__(self, data=None, left=None, right=None):
+    self.data = data
+    self.left = left
+    self.right = right
+
+
+  def tree_traversal(root):     #Time: O(n); Space: O(h); h-> height; min(h) = log n; max(h) = n
+    if root:
+      # Pre-order: Processes the root before the traversals of left and right childer.
+      print("Preorder: %d" %root.data)
+      
+      tree_traversal(root.left)
+      
+      # In-order: Processes the root after the traversal of left child and before the traversal of right chiLd.
+      print("Inorder: %d" %root.data)
+      
+      tree_traversal(root.right)
+      
+      # Post-order: Processes the root after the traversals of left and right chiLd.
+      print("Postorder: %d" %root.data)
+     
+  
