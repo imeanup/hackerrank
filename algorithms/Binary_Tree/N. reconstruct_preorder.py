9ce617249275f1f8d746ad03b0bb7058ba4ed4b5
anupam <tah.anupam@gmail.com>
2022-10-23 00:22:36 +0530

    Reconstruct Preorder

--- algorithms/Binary_Tree/N. reconstruct_preorder.py
+++ algorithms/Binary_Tree/N. reconstruct_preorder.py
@@ -1,3 +1,16 @@
 # Reconstruct a binary tree from a preorder traversal with markers
-def reconstruct_preorder(preorder):
-  ...
+
+
+def reconstruct_preorder(preorder):         # Time: O(n)
+    def reconstruct_preorder_helper(preorder_iter):
+        subtree_key = next(preorder_iter)
+        if subtree_key is None:
+            return None
+
+        # Note that reconstruct_preorder_helper updates preorder_iter. So the
+        # order of following two calls are critical.
+        left_subtree = reconstruct_preorder_helper(preorder_iter)
+        right_subtree = reconstruct_preorder_helper(preorder_iter)
+        return BinaryTreeNode(subtree_key, left_subtree, right_subtree)
+
+    return reconstruct_preorder_helper(iter(preorder))
