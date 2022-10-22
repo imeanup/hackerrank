fc2dd867dab3faf8c22b7222af8cf8fd146ca47a
anupam <tah.anupam@gmail.com>
2022-10-23 00:25:00 +0530

    Create List of Leaves

--- algorithms/Binary_Tree/O. create_list_of_leaves.py
+++ algorithms/Binary_Tree/O. create_list_of_leaves.py
@@ -1,4 +1,10 @@
 # From a linked list from the leaves of a binary tree
 
-def create_list_of_leaves(tree):
-  ...
+
+def create_list_of_leaves(tree):  # Time: O(n)
+    if not tree:
+        return []
+    if not tree.left and not tree.right:
+        return [tree]
+    # First do the left subtree, and then do the right subtree.
+    return create_list_of_leaves(tree.elft) + create_list_of_leaves(tree.right)
