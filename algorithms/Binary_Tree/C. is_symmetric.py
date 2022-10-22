4b674e58bdaf6459b2eadbed6c0f304aa5af66c0
anupam <tah.anupam@gmail.com>
2022-10-22 23:11:57 +0530

    "formatted"

--- algorithms/Binary_Tree/C. is_symmetric.py
+++ algorithms/Binary_Tree/C. is_symmetric.py
@@ -1,14 +1,17 @@
 # Testing if a Binary Tree is symmetric or asymmetric.
 
-def is_symmetric(tree):        # Time: O(n); Space: O(h)
-  def check_symmetric(subtree_0, subtree_1):
-    if not subtree_0 and not subtree_1:
-      return True
-    elif subtree_0 and subtree_1:
-      return (subtree_0.data == subtree_1.data
-              and check_symmetric(subtree_0.left, subtree_1.right)
-              and check_symmetric(subtree_0.right, subtree_1.left))
-    # One subtree is empty, and the other is not.
-    return False
-  
-  return not tree or check_symmetric(tree.left, tree.right)
+
+def is_symmetric(tree):  # Time: O(n); Space: O(h)
+    def check_symmetric(subtree_0, subtree_1):
+        if not subtree_0 and not subtree_1:
+            return True
+        elif subtree_0 and subtree_1:
+            return (
+                subtree_0.data == subtree_1.data
+                and check_symmetric(subtree_0.left, subtree_1.right)
+                and check_symmetric(subtree_0.right, subtree_1.left)
+            )
+        # One subtree is empty, and the other is not.
+        return False
+
+    return not tree or check_symmetric(tree.left, tree.right)
