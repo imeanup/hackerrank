d2f49eb9e86cdc3aa5343e9e82fad60c4e34397a
anupam <tah.anupam@gmail.com>
2022-10-23 13:24:29 +0530

    First greater than k

--- algorithms/Binary Search Tree/B. find-first-greater-than-k.py
+++ algorithms/Binary Search Tree/B. find-first-greater-than-k.py
@@ -1,1 +1,12 @@
 # Find the first key greater than a given value in a BST
+
+
+def find_first_greater_than_k(tree, k):
+    subtree, first_so_far = tree, None
+    while subtree:
+        if subtree.data > k:
+            first_so_far, subtree = subtree, subtree.left
+        else: # Root and all keys in left subtree are <= k, so skip them.
+            subtree = subtree.right
+
+    return first_so_far
