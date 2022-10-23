33c753732b7d7094f407042a91b452753f99c793
anupam <tah.anupam@gmail.com>
2022-10-23 09:31:40 +0530

    ABSqrt2

--- algorithms/Binary Search Tree/G. ABSqrt2.py
+++ algorithms/Binary Search Tree/G. ABSqrt2.py
@@ -1,6 +1,10 @@
 import math, bintrees
 
 
+from cgitb import reset
+import math, bintrees
+
+
 class ABSqrt2:
     def __init__(self, a, b):
         self.a, self.b = a, b
@@ -24,3 +28,17 @@ class ABSqrt2:
             candidates[ABSqrt2(next_smallest.a + 1, next_smallest.b)] = None
             candidates[ABSqrt2(next_smallest.a, next_smallest.b + 1)] = None
         return result
+
+    def generate_first_k_a_b_sqrt2(k):
+        # will store the first k numbers of the form a + b sqrt(2)
+        cand = [ABSqrt2(0, 0)]
+        i = j = 0
+        for _ in range(1, k):
+            cand_i_plus_1 = ABSqrt2(cand[i].a + 1, cand[i].b)
+            cand_j_plus_sqrt2 = ABSqrt2(cand[j].a, cand[j].b + 1)
+            cand.append(min(cand_i_plus_1, cand_j_plus_sqrt2))
+            if cand_i_plus_1.val == cand[-1].val:
+                i += 1
+            if cand_j_plus_sqrt2.val == cand[-1].val:
+                j += 1
+        return [a.val for a in cand]
