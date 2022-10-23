7408b452cae12654acd9fa3c84721715740f6340
anupam <tah.anupam@gmail.com>
2022-10-23 13:10:06 +0530

    Reconstruct a BST from Traversal data

--- algorithms/Binary Search Tree/E. rebuild-bst-from-preorder.py
+++ algorithms/Binary Search Tree/E. rebuild-bst-from-preorder.py
@@ -1,1 +1,22 @@
 # Reconstruct a BST from Traversal data
+
+from bintrees import BSTNode
+
+
+def rebuild_bst_from_preorder(preorder_sequence):
+    def rebuild_bst_from_preorder_on_value_range(lower_bound, upper_bound):
+        if root_idx[0] == len(preorder_sequence):
+            return None
+        root = preorder_sequence[root_idx[0]]
+        if not lower_bound <= root <= upper_bound:
+            return None
+        root_idx[0] += 1
+        # Note that rebuild_bst_from_preorder_on_value_range updates root_idx[0].
+        # So the order of following two calls are critical.
+        left_subtree = rebuild_bst_from_preorder_on_value_range(lower_bound, root)
+        right_subtree = rebuild_bst_from_preorder_on_value_range(root, upper_bound)
+        return BSTNode(root, left_subtree, right_subtree)
+
+    # Tracks current subtree.
+    root_idx = [0]
+    return rebuild_bst_from_preorder_on_value_range(float("-inf"), float("inf"))
