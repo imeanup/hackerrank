87cad98ef36ad171de091a25b6b13b63b07c0dd6
anupam <tah.anupam@gmail.com>
2022-10-23 13:16:56 +0530

    LCA in a BST

--- algorithms/Binary Search Tree/D. find-LCA.py
+++ algorithms/Binary Search Tree/D. find-LCA.py
@@ -1,1 +1,12 @@
 # Compute the LCA in a BST
+
+def find_LCA(tree, s, b):
+    while tree.data < s.data or tree.data > b.data:
+        # Keep searching since tree is outside of [s, b].
+        while tree.data < s.data:
+            tree = tree.right  # LCA must be in tree's right child.
+        while tree.data > b.data:
+            tree = tree.left  # LCA must be in trees' left child.
+
+    # Now, s.data <= tree.data && tree.data <= b.data.
+    return tree
