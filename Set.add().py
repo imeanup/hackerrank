dd89feec972df2108317e6c652cac6bb2331b902
anupam <tah.anupam@gmail.com>
2022-10-26 22:00:33 +0530

    Create Set.add().py

---
+++ Set.add().py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+result = set()
+for i in range(n):
+    val = input()
+    result.add(val)
+    
+print(len(result))
+    
