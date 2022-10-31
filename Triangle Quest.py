c15a2a06e39745f98afb87f356f5f3acb371660b
anupam <tah.anupam@gmail.com>
2022-10-31 22:42:20 +0530

    Create Triangle Quest.py

---
+++ Triangle Quest.py
@@ -0,0 +1,13 @@
+for i in range(1, int(input())):
+    print(10**i // 9*i)
+    
+for i in range(1, int(input())):
+  for j in range(i):
+      print(i, end="")
+  print()
+  
+# 5
+# 1
+# 22
+# 333
+# 4444
