2da4a735c21c64e1766a5efde16433c05d7c2d5a
anupam <tah.anupam@gmail.com>
2022-11-14 15:21:26 +0000

    Update

--- Standardize Mobile Number Using Decorators.py
+++ Standardize Mobile Number Using Decorators.py
@@ -3,3 +3,12 @@ def wrapper(f):
         # complete the function
         f("+91 "+c[-10:-5]+" "+c[-5:] for c in l)
     return fun
+
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
