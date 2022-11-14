e0d5f2f614545d64a9027c1252b271c5709237a2
anupam <tah.anupam@gmail.com>
2022-11-14 15:25:18 +0000

    Nov 14, 2022, 3:25 PM

---
+++ Find Digits.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    div = ""
+    for i in str(n):
+        if int(i) != 0 and n % int(i) == 0:
+            div += i
+    return len(div)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
