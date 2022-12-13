db00e534aacfdf0c744a768655437f25484f66ce
anupam <tah.anupam@gmail.com>
2022-12-13 10:19:18 +0000

    Dec 13, 2022, 10:19 AM

--- Manachers Algorithm.py
+++ Manachers Algorithm.py
@@ -1,5 +1,25 @@
-class ManacherAlgorithm:
+def manacherAlg(s:str) -> str:
+    T = '#'.join('^{}$'.format(s))
+    n = len(T)
 
-    def longestPalindrome(self, s:str) -> str:
-        T = '#'.join('^{}$'.format(s))
+    P = [0]*n
+    C = 0
+    R = 0
+
+    for i in range(1, n-1):
+        P[i] = (R > i) and min(R-i, P[2*C - i])
+
+        while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+            P[i] += 1
+
+        if i + P[i] > R:
+            C = i
+            R = i + P[i]
         
+    maxLen, cenIdx = max((n, i) for i, n in enumerate(P))
+
+    return s[(cenIdx - maxLen)//2:(cenIdx + maxLen)//2]
+
+s = "babad"
+val = manacherAlg(s)
+print(val)
