db16d11411613ce36a6c312d082d3a92f3d8f316
anupam <tah.anupam@gmail.com>
2022-12-18 17:18:08 +0000

    Updated HeapSort.py

--- algorithms/HeapSort.py
+++ algorithms/HeapSort.py
@@ -1,70 +1,71 @@
 def PARENT(i):
-    return (i//2)
+    p = (i - 1) // 2
+    return p if 0 < i else i
 
 
-def LEFT(i):
-    return (2*i)
+def LEFT(i, n):
+    l = 2 * i + 1
+    return l if l < n else i
 
 
-def RIGHT(i):
-    return (2*i + 1)
+def RIGHT(i, n):
+    r = 2 * i + 2
+    return r if r < n else i
 
 
-def MAX_HEAPIFY(A, i):
-    l = LEFT(i)
-    r = RIGHT(i)
-    if l < len(A) and A[l] > A[i]:
+def MAX_HEAPIFY(A, n, i):
+    l = LEFT(i, n)
+    r = RIGHT(i, n)
+    if l < n and A[l] > A[i]:
         largest = l
     else:
         largest = i
-    if r < len(A) and A[r] > A[largest]:
+    if r < n and A[r] > A[largest]:
         largest = r
     if largest != i:
         A[i], A[largest] = A[largest], A[i]
-        MAX_HEAPIFY(A, largest)
+        MAX_HEAPIFY(A, n, largest)
 
 
-def MIN_HEAPIFY(A, i):
-    l = LEFT(i)
-    r = RIGHT(i)
-    if l < len(A) and A[l] < A[i]:
+def MIN_HEAPIFY(A, n, i):
+    l = LEFT(i, n)
+    r = RIGHT(i, n)
+    if l < n and A[l] < A[i]:
         smallest = l
     else:
         smallest = i
-    if r < len(A) and A[r] < A[smallest]:
+    if r < n and A[r] < A[smallest]:
         smallest = r
     if smallest != i:
         A[i], A[smallest] = A[smallest], A[i]
-        MIN_HEAPIFY(A, smallest)
+        MIN_HEAPIFY(A, n, smallest)
 
 
-def BUILD_MAX_HEAP(A):
-    n = len(A)
-    for i in range((n//2) - 1, -1, -1):
-        MAX_HEAPIFY(A, i)
+def BUILD_MAX_HEAP(A, n):
+    for i in range((n//2)-1, -1, -1):
+        MAX_HEAPIFY(A, n, i)
 
 
-def BUILD_MIN_HEAP(A):
-    n = len(A)
-    for i in range((n//2) - 1, -1, -1):
-        MIN_HEAPIFY(A, i)
+def BUILD_MIN_HEAP(A, n):
+    for i in range((n//2)-1, -1, -1):
+        MIN_HEAPIFY(A, n, i)
 
 
-def MAX_HEAPSORT(A):
-    BUILD_MAX_HEAP(A)
+def MAX_HEAPSORT(A, n):
+    BUILD_MAX_HEAP(A, n)
     print("\nMAX_heapsort\n")
-    for i in range(len(A) - 1, 0, -1):
+    for i in range(n-1, 0, -1):
         A[0], A[i] = A[i], A[0]
-        MAX_HEAPIFY(A, i)
+        MAX_HEAPIFY(A, i, 0)
         print(A)
 
 
-def MIN_HEAPSORT(A):
-    BUILD_MIN_HEAP(A)
+def MIN_HEAPSORT(A, n):
+    BUILD_MIN_HEAP(A, n)
     print("\nMIN_heapsort\n")
-    for i in range(len(A) - 1, 0, -1):
+    for i in range(n-1, 0, -1):
         A[0], A[i] = A[i], A[0]
-        MIN_HEAPIFY(A, i)
+        MIN_HEAPIFY(A, i, 0)
         print(A)
 
 
@@ -74,8 +75,8 @@ if __name__ == "__main__":
     # A = [x for x in range(-10, 11)]
     A = [3, 2, 3, 1, 2, 4, 5, 5, 6]
     shuffle(A)
-    MAX_HEAPSORT(A)
-    MIN_HEAPSORT(A)
+    MAX_HEAPSORT(A, len(A))
+    MIN_HEAPSORT(A, len(A))
 
 
 
