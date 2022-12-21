224b98a4bf029a7cf7bf55e609ca2cfa6235e69f
anupam <tah.anupam@gmail.com>
2022-12-21 17:30:36 +0000

    Recursive Matrix mult

--- algorithms/DivideAndConquer/MatrixMultiplication.py
+++ algorithms/DivideAndConquer/MatrixMultiplication.py
@@ -8,8 +8,25 @@ def SQUARE_MATRIX_MULTIPLY(A, B):
                 C[i][j] += A[i][k] * B[k][j]
     return C
 
+def SQUARE_MATRIX_MULTIPLY_RECURSIVE(A, B):
+    # m = len(A)
+    # n = len(B[0])
+    C = [[0 for i in range(2)] for j in range(2)]
+    # if m == 0:
+    #     C[0][0] = A[0][0] * B[0][0]
+
+    # else:
+    C[0][0] = SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[0][0], B[0][0]) + SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[0][1], B[1][0])
+    C[0][1] = SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[0][0], B[0][1]) + SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[1][0], B[1][1])
+    C[1][0] = SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[1][0], B[0][0]) + SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[1][1], B[1][1])
+    C[1][1] = SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[1][0], B[0][1]) + SQUARE_MATRIX_MULTIPLY_RECURSIVE(A[1][1], B[1][1])
+
+    return C
+
+
 if __name__ == "__main__":
     A = [[1, 3], [7, 5]]
     B = [[6, 8], [4, 2]]
     C = SQUARE_MATRIX_MULTIPLY(A, B)
-    print(C)
+    D = SQUARE_MATRIX_MULTIPLY_RECURSIVE(A, B)
+    print(D)
