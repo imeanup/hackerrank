ae5b0ddcc921b6a59c8d33c1f935bdb5d05ed41c
anupam <tah.anupam@gmail.com>
2022-12-26 18:25:24 +0000

    Renamed BubbleSort

--- algorithms/Sorting/BubbleSort.py
+++ algorithms/Sorting/BubbleSort.py
@@ -11,13 +11,13 @@ for i = 1 to A.length - 1
             exchange A[j] with A[j - 1]        
 '''
 
-def bubbleSort(A):
+def BUBBLESORT(A):
     for i in range(len(A)-1):
         for j in range(len(A)-1, i, -1):
             if A[j] < A[j - 1]:
                 A[j], A[j-1] = A[j-1], A[j]
     
-def bubble_sort(A):
+def _BUBBLESORT(A):
     l = len(A)
     for i in range(l):
         for j in range(0, l-i-1):
@@ -29,5 +29,5 @@ if __name__=="__main__":
     A = [x for x in range(-51, 51)]
     shuffle(A)
     print(f"\nArray\n\n{A}")
-    bubble_sort(A)
+    _BUBBLESORT(A)
     print(f"\nArray\n\n{A}\n")
