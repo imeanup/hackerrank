1e851e2e16de383c35a13208dd6df6e33dbc3ecc
anupam <tah.anupam@gmail.com>
2023-01-11 00:58:17 +0530

    Update 30. Substring with Concatenation of All Words.py
    #TODO: Try to improve using sliding window i.e. j-i+1 < k and j-i+1 == k

--- leetcode/30. Substring with Concatenation of All Words.py
+++ leetcode/30. Substring with Concatenation of All Words.py
@@ -1,3 +1,34 @@
 '''
 https://leetcode.com/problems/substring-with-concatenation-of-all-words/
 '''
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if not s or not words: 
+            return []
+        n, m, k = len(s), len(words), len(words[0])
+        
+        res = []
+        words_map = defaultdict(int)
+        
+        for word in words:
+            words_map[word] += 1
+        i, j = 0, 0
+        
+        while i < n:
+            j = i
+            words_map_copy = words_map.copy()
+            for _ in range(m):
+                temp = s[j:j+k]
+                if temp in words_map_copy:
+                    words_map_copy[temp] -= 1
+                    if words_map_copy[temp] == 0:
+                        del words_map_copy[temp]
+                else:
+                    break
+                j += k
+            if not words_map_copy:
+                res.append(i)
+            i += 1
+        return res
+
