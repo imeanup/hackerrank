0b6f70c2e1474b82475a773d21903a78c2b32829
anupam <tah.anupam@gmail.com>
2023-01-12 20:32:20 +0530

    Update 1519. Number of Nodes in the Sub-Tree With the Same Label.py

--- leetcode/1519. Number of Nodes in the Sub-Tree With the Same Label.py
+++ leetcode/1519. Number of Nodes in the Sub-Tree With the Same Label.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/contest/weekly-contest-198/
+# https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/description/
+
 from typing import List
 
 class Solution:
@@ -32,3 +35,31 @@ class Solution:
         # Start the DFS from the root node
         dfs(0, -1)
         return res
+
+from collections import defaultdict
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        visited = set() 
+        ans = [0] * n
+        
+        def dfs(node, labels):
+            visited.add(node)
+            count = [0]*26
+            for u in graph[node]:
+                if u not in visited:
+                    gCount = dfs(u, labels)
+                    for i in range(26):
+                        count[i] += gCount[i]
+            ch = labels[node]
+            count[ord(ch)-ord('a')] += 1
+            ans[node] = count[ord(ch)-ord('a')]
+            return count
+        
+        dfs(0, labels)
+
+        return ans
