8c29d602631844bf53c083964bf9a6098773fa08
anupam <tah.anupam@gmail.com>
2023-01-16 21:10:21 +0530

    Update UnionFind.py

--- algorithms/union_find/UnionFind.py
+++ algorithms/union_find/UnionFind.py
@@ -2,21 +2,25 @@
 
 # UnionFind class
 class UnionFind:
+    '''
+    create an array of size N with the values equal to the corresponding array indices; requires linear time.
+    O(N) space complexity
+    '''
     def __init__(self, size):
         self.root = [i for i in range(size)]
 
-    def find(self, x):
+    def find(self, x): # O(1)
         return self.root[x]
 		
-    def union(self, x, y):
+    def union(self, x, y): 
         rootX = self.find(x)
         rootY = self.find(y)
         if rootX != rootY:
-            for i in range(len(self.root)):
+            for i in range(len(self.root)): # O(N)
                 if self.root[i] == rootY:
                     self.root[i] = rootX
 
-    def connected(self, x, y):
+    def connected(self, x, y): # O(1)
         return self.find(x) == self.find(y)
 
 
