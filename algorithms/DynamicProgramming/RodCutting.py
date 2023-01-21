cfb082f11366ee2c51332110d988788f90f981a6
anupam <tah.anupam@gmail.com>
2023-01-21 10:02:49 +0530

    Update RodCutting.py

--- algorithms/DynamicProgramming/RodCutting.py
+++ algorithms/DynamicProgramming/RodCutting.py
@@ -57,13 +57,50 @@ def PRINT_CUT_ROD_SOLUTION(p, n):
         n = n - s[n]
 
 
+"""
+Ex 15.1-4
+"""
+
+
+def MEMOIZED_CUT_ROD_WITH_SOLUTION(p, n):
+    r = [-float("inf") for _ in range(n + 1)]
+    s = [0 for _ in range(n + 1)]
+    return MEMOIZED_CUT_ROD_AUX_WITH_SOLUTION(p, n, r, s)
+
+
+def MEMOIZED_CUT_ROD_AUX_WITH_SOLUTION(p, n, r, s):
+    if r[n] >= 0:
+        return (r[n], s)
+    if n == 0:
+        q = 0
+    else:
+        q = -float("inf")
+        for i in range(1, n + 1):
+            result = MEMOIZED_CUT_ROD_AUX_WITH_SOLUTION(p, n - i, r, s)
+            if q < p[i - 1] + result[0]:
+                q = p[i - 1] + result[0]
+                s[n] = i
+    r[n] = q
+    return (r[n], s)
+
+
+def PRINT_CUT_ROD_SOLUTION(p, n):
+    r, s = MEMOIZED_CUT_ROD_WITH_SOLUTION(p, n)
+    while n > 0:
+        print(s[n])
+        n = n - s[n]
+
+
 if __name__ == "__main__":
     p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
     n = int(input("Length: "))
-    print("Cut Rod: ", CUT_ROD(p, n - 1))
-    print("Memoized Cut Rod: ", MEMOIZED_CUT_ROD(p, n - 1))
-    print("Bottom Up: ", BOTTOM_UP_CUT_ROD(p, n - 1))
-    r, s = EXTENDED_BOTTOM_UP_CUT_ROD(p, n - 1)
-    print(r[n - 1])
-    print(s)
-    print("Extended Bottom Up: ", PRINT_CUT_ROD_SOLUTION(p, n))
+    # print("Cut Rod: ", CUT_ROD(p, n - 1))
+    # print("Memoized Cut Rod: ", MEMOIZED_CUT_ROD(p, n - 1))
+    # print("Bottom Up: ", BOTTOM_UP_CUT_ROD(p, n - 1))
+    # r, s = EXTENDED_BOTTOM_UP_CUT_ROD(p, n - 1)
+    # print(r[n - 1])
+    # print(s)
+    # print("Extended Bottom Up: ", PRINT_CUT_ROD_SOLUTION(p, n))
+    result = MEMOIZED_CUT_ROD_WITH_SOLUTION(p, n)
+    print(result[0])
+    print(result[1])
