2cb40f84a26ec476abce7ebe5503b528863bf91b
Anupam <tah.anupam@gmail.com>
2023-04-25 12:19:42 +0530

    Update 2336. Smallest Number in Infinite Set.py

--- leetcode/2336. Smallest Number in Infinite Set.py
+++ leetcode/2336. Smallest Number in Infinite Set.py
@@ -6,6 +6,41 @@ class SmallestInfiniteSet:
         self.counter = 1
 
     def popSmallest(self) -> int:
+        # print(self.container)
+        if (self.container):
+            minimum = min(self.isPresent)
+            self.container.remove(minimum)
+            self.isPresent.remove(minimum)   
+        else:
+            minimum = self.counter
+            self.counter += 1
+        # print(minimum)
+        return minimum
+
+    def addBack(self, num: int) -> None:
+        if self.counter <= num or num in self.container:
+            return
+        else:
+            self.container.append(num)
+            self.isPresent.add(num)
+            # print(self.container)
+        
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
+
+# solution from editorial
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.isPresent = set()
+        self.container = []
+        self.counter = 1
+
+    def popSmallest(self) -> int:
         if len(self.container):
             minimum = heapq.heappop(self.container)
             self.isPresent.remove(minimum)
