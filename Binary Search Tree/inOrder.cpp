ed93aa5554ad3dc7911ad8d2caf895705fb8bb8a
Anupam <tah.anupam@gmail.com>
2023-05-09 23:53:14 +0530

    Update inOrder.cpp

--- Binary Search Tree/inOrder.cpp
+++ Binary Search Tree/inOrder.cpp
@@ -1,1 +1,34 @@
+#include "bst.h"
+#include <stack>
 
+void BST :: inorder(Node *root){
+    stack<Node*> s;
+    Node *ptr = root;
+    if (!ptr){
+        cout << "Tree is empty\n";
+        return;
+    }
+    while(true){
+        while (ptr->left != nullptr){
+            // cout << " " << ptr->value; // In stack
+            s.push(ptr);
+            ptr = ptr->left;
+        }
+        while (ptr->right == nullptr){
+            cout << " " << ptr->value;
+            if (s.empty()){
+                return;
+            }
+            ptr = s.top();
+            // cout << " " << ptr->value; // Out-of stack
+            s.pop();
+        }
+        cout << " " << ptr->value;
+        ptr = ptr->right;
+    }
+    cout << endl;
+}
+
+void BST::inorder() {
+    inorder(root);
+}
