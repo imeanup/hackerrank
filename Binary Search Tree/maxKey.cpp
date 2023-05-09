f4f9ac0edf3cb84611588917eb577c84d4394dde
Anupam <tah.anupam@gmail.com>
2023-05-09 23:57:17 +0530

    Update maxKey.cpp

--- Binary Search Tree/maxKey.cpp
+++ Binary Search Tree/maxKey.cpp
@@ -1,1 +1,12 @@
+#include "bst.h"
 
+int BST :: findMax() {
+    if (!root){
+      cout << "Tree is empty\n";
+    }
+    Node *current = root;
+    while (current->right != nullptr) {
+        current = current->right;
+    }
+    return current->value;
+}
