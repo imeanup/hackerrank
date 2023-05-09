fc4ff916efdc9fe42f3ee22435d6fb7ac116832c
Anupam <tah.anupam@gmail.com>
2023-05-09 23:23:24 +0530

    Create main.cpp

---
+++ Binary Search Tree/main.cpp
@@ -0,0 +1,33 @@
+// main.cpp
+#include <iostream>
+#include "BST.h"
+using namespace std;
+
+int main() {
+    BST tree;
+    // Tree insertion
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(60);
+    tree.insert(38);
+    tree.insert(35); 
+    tree.insert(55); 
+    tree.insert(22); 
+    tree.insert(59); 
+    tree.insert(94); 
+    tree.insert(13); 
+    tree.insert(98); 
+
+    cout << "Inorder Traversal: ";
+    tree.inorder();
+    cout << "\n";
+
+    /*
+    cout << "Level Order: ";
+    tree.levelOrder();
+    */
+    
+    return 0;
+}
+// This command need to be updated
+// g++ main.cpp BST\ Deletion.cc BST\ Insertion.cc Traversal\ Inorder.cc BST.cc Level\ Order\ Traversal.cc -o main
