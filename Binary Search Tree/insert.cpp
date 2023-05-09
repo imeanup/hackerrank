c40a0e7a4e122449a5d632b9ed4471bb7fd473bc
Anupam <tah.anupam@gmail.com>
2023-05-09 23:52:43 +0530

    Update insert.cpp

--- Binary Search Tree/insert.cpp
+++ Binary Search Tree/insert.cpp
@@ -1,1 +1,39 @@
+#include "bst.h"
 
+Node* BST:: insert(Node *root, int key){
+    Node *tmp, *par, *ptr;
+    ptr = root;
+    par = nullptr;
+    while (ptr != nullptr){
+        par = ptr;
+        if (key < ptr->value){
+            ptr = ptr->left;
+        }
+        else if (key > ptr->value){
+            ptr = ptr->right;
+        }
+        else{
+            cout << "Duplicate Key";
+            return root;
+        }
+    }
+    tmp = new Node();
+    tmp->value = key;
+    tmp->left = nullptr;
+    tmp->right = nullptr;
+
+    if (!par){
+        root = tmp;
+    }
+    else if(key < par->value){
+        par->left = tmp;
+    }
+    else{
+        par->right = tmp;
+    }
+    return root;
+}
+
+void BST:: insert(int key) {
+    root = insert(root, key);
+}
