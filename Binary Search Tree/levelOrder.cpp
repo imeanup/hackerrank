fc5401689d0ee515939a07ff50d72ec9b0e74678
Anupam <tah.anupam@gmail.com>
2023-05-09 23:33:34 +0530

    Update levelOrder.cpp

--- Binary Search Tree/levelOrder.cpp
+++ Binary Search Tree/levelOrder.cpp
@@ -1,1 +1,33 @@
+#include "BST.h"
+#include <queue>
 
+void BST :: levelOrder(Node *root){
+    if (!root){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    int level = 0;
+    while (!q.empty()){
+        int level_size = q.size();
+        cout << "Level: " << level << " -> " ;
+        for (int i = 0; i < level_size; i++){
+            Node *tmp = q.front();
+            q.pop();
+            
+            cout << tmp->value << " ";
+            if (tmp->left){
+                q.push(tmp->left);
+            }
+            if (tmp->right){
+                q.push(tmp->right);
+            }
+        }  
+        cout << endl;
+        level++;
+    }
+}
+
+void BST :: levelOrder(){
+    levelOrder(root);
+}
