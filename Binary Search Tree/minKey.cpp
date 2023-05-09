e4d0beb6c695e54688a02c869c5c9672f0d0ee0b
Anupam <tah.anupam@gmail.com>
2023-05-09 23:56:27 +0530

    Update minKey.cpp

--- Binary Search Tree/minKey.cpp
+++ Binary Search Tree/minKey.cpp
@@ -1,1 +1,12 @@
+#include "bst.h"
 
+int BST :: findMin() {
+    if (!root){
+      cout << "Tree is empty\n";
+    }
+    Node *current = root;
+    while (current->left != nullptr) {
+        current = current->left;
+    }
+    return current->value;
+}
