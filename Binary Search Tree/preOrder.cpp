42ca6cb0410afb2ee37d2656e9464567a62ffbdc
Anupam <tah.anupam@gmail.com>
2023-05-09 23:40:13 +0530

    Update preOrder.cpp

--- Binary Search Tree/preOrder.cpp
+++ Binary Search Tree/preOrder.cpp
@@ -1,1 +1,28 @@
+#include "bst.h"
+#include <stack>
 
+void BST :: preorder(Node *root){
+    stack<Node*> s;
+    Node *ptr = root;
+    if (!ptr){
+        cout << "Tree is empty\n";
+        return;
+    }
+    s.push(ptr);
+    while (!s.empty()){
+        ptr = s.top();
+        s.pop();
+        cout << " " << ptr->value;
+        if (ptr->right != nullptr){
+            s.push(ptr->right);
+        }
+        if(ptr->left != nullptr){
+            s.push(ptr->left);
+        }
+    }
+    cout << endl;
+}
+
+void BST :: preorder(){
+  preorder(root);
+}
