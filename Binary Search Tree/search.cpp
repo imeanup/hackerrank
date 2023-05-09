44f21a9a649babb37c9f23d2751d4848c0e18ba9
Anupam <tah.anupam@gmail.com>
2023-05-09 23:51:43 +0530

    Update search.cpp

--- Binary Search Tree/search.cpp
+++ Binary Search Tree/search.cpp
@@ -1,1 +1,20 @@
+#include "bst.h"
 
+Node *BST :: search(Node *root, int key){
+  while (root){
+    if (key < root->value){
+      root = root->left;
+    }
+    else if (key > root->value){
+      root = root->right;
+    }
+    else{
+      return root;
+    }
+  }
+  return nullptr; 
+}
+
+bool BST :: search(int key) {
+    return search(root, key) != nullptr;
+}
