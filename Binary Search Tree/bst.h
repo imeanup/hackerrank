c6a1ac6d7b266cac03ca172b238f943ef345e9a4
Anupam <tah.anupam@gmail.com>
2023-05-09 23:31:35 +0530

    Update bst.h

--- Binary Search Tree/bst.h
+++ Binary Search Tree/bst.h
@@ -9,20 +9,27 @@ struct Node {
     int value;
     Node *left;
     Node *right;
+    Node(int value): value(value), left(nullptr), right(nullptr) {}
 };
 
 class BST {
 private:
     Node *root;
-    Node* insert(Node *root, int key);
-    Node *deletion(Node *root, int key);
+    Node *insert(Node *root, int key);
+    Node *remove(Node *root, int key);
+    Node *search(Node *root, int key);
     void inorder(Node *root);
+    void preorder(Node *root);
+    void postorder(Node *root);
     void levelOrder(Node *root);
 public:
     BST();
+    void search(int key);
     void insert(int key);
-    void deletion(int key);
+    void remove(int key);
     void inorder();
+    void preorder();
+    void postorder();
     void levelOrder();
 };
 
