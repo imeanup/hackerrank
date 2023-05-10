cb4de4e93b2b0b41699fdb543047f2a4b8f5c028
Anupam <tah.anupam@gmail.com>
2023-05-10 15:00:04 +0000

    May 10, 2023, 3:00 PM

--- Threaded Binary Tree/tbt.h
+++ Threaded Binary Tree/tbt.h
@@ -11,4 +11,23 @@ struct Node{
     Node *left, *right;
     bool rightThread, leftThread;
 };
+
+class TBT{
+    private:
+        Node *root;
+        Node *insert(Node *root, int key);
+        Node *remove(Node *root, int key);
+        void inorder(Node *root);
+        void preorder(Node * root);
+        void successor(Node *root);
+        void predecessor(Node *root);
+    public:
+        TBT();
+        void insert(int key);
+        void remove(int key);
+        void inorder();
+        void preorder();
+        void successor();
+        void predecessor();
+};
 #endif
