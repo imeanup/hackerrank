1714d659fb5bf328dc787cf9fe3d9bdd0b5060c4
Anupam <tah.anupam@gmail.com>
2023-05-12 00:04:19 +0530

    Update insert.cpp

--- Threaded Binary Tree/insert.cpp
+++ Threaded Binary Tree/insert.cpp
@@ -1,1 +1,44 @@
-// TODO
+// insert.cpp
+
+#include "tbt.h"
+
+Node *TBT :: insert(Node *root, int key){
+    Node *tmp, *par, *ptr;
+    int found = 0;
+    ptr = root;
+    par = nullptr;
+    while (ptr){
+        if (key == ptr->val){
+            found = 1;
+            break;
+        }
+        par = ptr;
+        if (key < ptr->val){
+            if (!ptr->leftThread){
+                ptr = ptr->left;
+            }
+            else{
+                break;
+            }
+        }
+        else{
+            if (!ptr->rightThread){
+                ptr = ptr->right;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    if (found){
+        cout << "Duplicate Key\n";
+    }
+    else{
+        tmp = new Node;
+        // TODO
+    }
+}
+
+void TBT :: insert(int key){
+    root = insert(root, key);
+}
