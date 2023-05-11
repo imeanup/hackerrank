a0e760c7b18231f26a4f04c7b5c2bd9ac5650464
Anupam <tah.anupam@gmail.com>
2023-05-12 00:03:42 +0530

    Update inorder.cpp

--- Threaded Binary Tree/inorder.cpp
+++ Threaded Binary Tree/inorder.cpp
@@ -1,8 +1,21 @@
 // inorder.cpp
+
 #include "tbt.h"
 
 void TBT :: inorder(Node *root){
-    // TODO
+    Node *ptr;
+    if (!root){
+        cout << "Tree is empty\n";
+        return;
+    }
+    ptr = root;
+    while (!ptr->leftThread){
+        ptr = ptr->left;
+    }
+    while (ptr){
+        cout << ptr->val << " ";
+        ptr = successor(ptr);
+    }
 }
 
 void TBT :: inorder() {
