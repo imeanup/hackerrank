9fb4bd06278a015d9a3636c6325710f8e69c6fd2
Anupam <tah.anupam@gmail.com>
2023-05-12 00:03:23 +0530

    Update preorder.cpp

--- Threaded Binary Tree/preorder.cpp
+++ Threaded Binary Tree/preorder.cpp
@@ -1,1 +1,33 @@
-// TODO
+// preorder.cpp
+
+#include "tbt.h"
+
+void TBT :: preorder(Node *root){
+    Node *ptr;
+    if (!root){
+        cout << "Tree is empty\n";
+        return;
+    }
+    ptr = root;
+    while (ptr){
+        cout << ptr->val << " ";
+        if (!ptr->leftThread){
+            ptr = ptr->left;
+        }
+        else if (!ptr->rightThread){
+            ptr = ptr->right;
+        }
+        else{
+            while (ptr && ptr->rightThread){
+                ptr = ptr->right;
+            }
+            if (ptr){
+                ptr = ptr->right;
+            }
+        }
+    }
+}
+
+void TBT :: preorder(){
+    preorder(root);
+}
