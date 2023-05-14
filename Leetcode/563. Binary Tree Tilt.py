6352c53b715b976aae7d18d632042e7a98a95705
Anupam <tah.anupam@gmail.com>
2023-05-14 23:35:38 +0530

    Create 563. Binary Tree Tilt.py

---
+++ Leetcode/563. Binary Tree Tilt.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        total_tilt = 0
+
+        def valueSum(node):
+            nonlocal total_tilt
+
+            if not node:
+                return 0
+
+            left_sum = valueSum(node.left)
+            right_sum = valueSum(node.right)
+            tilt = abs(left_sum - right_sum)
+            total_tilt += tilt
+
+            return left_sum + right_sum + node.val
+
+        valueSum(root)
+
+        return total_tilt
