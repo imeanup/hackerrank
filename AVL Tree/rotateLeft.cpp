f16fc3ea493ba633f75689d862470f31e9e02f67
Anupam <tah.anupam@gmail.com>
2023-05-23 14:32:53 +0530

    Update rotateLeft.cpp

--- AVL Tree/rotateLeft.cpp
+++ AVL Tree/rotateLeft.cpp
@@ -1,1 +1,11 @@
+// leftRotate.cpp
 
+#include "avl.h"
+
+Node *AVL :: leftRotate(Node *pptr){
+    Node *aptr;
+    aptr = pptr->right;
+    pptr->right = aptr->left;
+    aptr->left = pptr;
+    return aptr;
+}
