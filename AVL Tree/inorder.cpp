25994cf51e22eb8f26ccf6a4f206f42cd039b99e
Anupam <tah.anupam@gmail.com>
2023-05-23 14:34:03 +0530

    Update inorder.cpp

--- AVL Tree/inorder.cpp
+++ AVL Tree/inorder.cpp
@@ -1,1 +1,15 @@
+// inorder.cpp
 
+#include "avl.h"
+
+void AVL :: inorder(Node *root){
+    if (root) {
+        inorder(root->left);
+        cout << root->val << " ";
+        inorder(root->right);
+    }
+}
+
+void AVL::inorder() {
+    inorder(root);
+}
