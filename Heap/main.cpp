54d252c15d18bb729ec89ebe51ae652e473eb5d7
Anupam <tah.anupam@gmail.com>
2023-06-03 00:20:40 +0530

    Update main.cpp

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -1,1 +1,35 @@
-// TODO: Directory Created
+// main.cpp
+#include "heap.h"
+
+int main() {
+    int arr[] = {10, 5, 3, 2, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    buildHeap(arr, n);
+
+    cout << "Max-Heap: ";
+    printArray(arr, n);
+
+    insert(arr, n, 15);
+
+    cout << "After inserting 15: ";
+    printArray(arr, n);
+
+    deleteRoot(arr, n);
+
+    cout << "After deleting root: ";
+    printArray(arr, n);
+
+    return 0;
+}
+
+
+// MAX Heap 
+
+/* 
+g++ heapify.cpp delete.cpp build.cpp insert.cpp print.cpp main.cpp -o output
+./output 
+Max-Heap: 10 5 3 2 4 
+After inserting 15: 15 5 10 2 4 3 
+After deleting root: 10 5 3 2 4 
+*/
