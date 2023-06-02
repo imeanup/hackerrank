b7621e9ec846d1c3e1336d197f0ec395624bd328
Anupam <tah.anupam@gmail.com>
2023-06-03 00:21:05 +0530

    Create heapify.cpp

---
+++ Heap/heapify.cpp
@@ -0,0 +1,17 @@
+// heapify.cpp
+
+#include "heap.h"
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
