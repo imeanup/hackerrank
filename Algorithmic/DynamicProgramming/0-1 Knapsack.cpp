395724233b4e27587453823c0d4796f26a84d18b
Anupam <tah.anupam@gmail.com>
2023-06-07 21:21:00 +0530

    Update 0-1 Knapsack.cpp

--- Algorithmic/DynamicProgramming/0-1 Knapsack.cpp
+++ Algorithmic/DynamicProgramming/0-1 Knapsack.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int knapSack(int W, vector<int> wt, vector<int> val, int n) {
+    vector<vector<int> > K(n + 1, vector<int>(W + 1));
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
+}
+
+int main() {
+    vector<int> val = {60, 100, 120};
+    vector<int> wt = {10, 20, 30};
+    int W = 50;
+    int n = val.size();
+    cout << knapSack(W, wt, val, n);
+    return 0;
+}
