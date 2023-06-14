7c98b565c5c9075591d20c80b01f6dbdeaa350be
Anupam <tah.anupam@gmail.com>
2023-06-14 23:59:04 +0530

    Update 993. Cousins in Binary Tree.py

--- Leetcode/993. Cousins in Binary Tree.py
+++ Leetcode/993. Cousins in Binary Tree.py
@@ -28,3 +28,32 @@ class Solution:
 
         levelOrder(root)
         return xDepth == yDepth and xPar != yPar
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        info = {}
+        def levelOrder(node):
+            if not node:
+                return
+            q = []
+            level = 0
+            q.append((node, None))
+            while q:
+                levelSize = len(q)
+                for i in range(levelSize):
+                    tmp, parent = q.pop(0)
+                    info[tmp.val] = (level, parent)
+                    if (tmp.left): q.append((tmp.left, tmp))
+                    if (tmp.right): q.append((tmp.right, tmp))
+                level += 1
+
+        levelOrder(root)
+        # print(info)
+        
+        return info[x][0] == info[y][0] and info[x][1] != info[y][1]
