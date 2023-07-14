45a80d792001fb511b186a7e7d41fb56bcaf0201
Anupam <tah.anupam@gmail.com>
2023-07-14 22:30:13 +0530

    Update 65. Valid Number.cpp

--- Leetcode/65. Valid Number.cpp
+++ Leetcode/65. Valid Number.cpp
@@ -36,3 +36,39 @@ public:
 // ".1"
 // "3E+7"
 // "+6e-1"
+/*
+// --- Other --- comment: https://leetcode.com/problems/valid-number/solutions/23728/a-simple-solution-in-python-based-on-dfa/
+// https://www.youtube.com/watch?v=RYNN-tb9WxI
+class Solution(object):
+  def isNumber(self, s):
+      """
+      :type s: str
+      :rtype: bool
+      """
+      #define a DFA
+      state = [{}, 
+              {'blank': 1, 'sign': 2, 'digit':3, '.':4}, 
+              {'digit':3, '.':4},
+              {'digit':3, '.':5, 'e':6, 'blank':9, 'E': 6},
+              {'digit':5},
+              {'digit':5, 'e':6, 'blank':9},
+              {'sign':7, 'digit':8},
+              {'digit':8},
+              {'digit':8, 'blank':9},
+              {'blank':9}]
+      currentState = 1
+      for c in s:
+          if c >= '0' and c <= '9':
+              c = 'digit'
+          if c == ' ':
+              c = 'blank'
+          if c in ['+', '-']:
+              c = 'sign'
+          if c not in state[currentState].keys():
+              return False
+          currentState = state[currentState][c]
+      if currentState not in [3,5,8,9]:
+          return False
+      return True
+
+*/
