49c3493e0e083752106a2ba06cd26cf49cd139d6
Anupam <tah.anupam@gmail.com>
2024-04-18 08:11:35 +0530

    Update A_Ferris_Wheel.cpp

--- AtCoder.jp/ABC/127/A_Ferris_Wheel.cpp
+++ AtCoder.jp/ABC/127/A_Ferris_Wheel.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     int a, b; cin >> a >> b;
-    if (a >= 0 && a <= 5) cout << 0 << "\n";
-    else if (a >= 6 && a <= 12) cout << b/2 << "\n";
+    if (a < 6) cout << 0 << "\n";
+    else if (a < 13) cout << b/2 << "\n";
     else cout << b << "\n";
     return 0;
 }
