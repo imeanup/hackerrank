c4488d38ae05ea39222cbc85ced276ef28b2120c
Anupam <tah.anupam@gmail.com>
2024-05-27 10:03:02 +0530

    Update D_Intersecting_Intervals.cpp

--- AtCoder.jp/ABC/355/D_Intersecting_Intervals.cpp
+++ AtCoder.jp/ABC/355/D_Intersecting_Intervals.cpp
@@ -5,6 +5,41 @@ using ll = int64_t;
 #define repi(i, x, n) for(int i=x;i<(n);i++)
 #define all(x) x.begin(), x.end()
 
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+#define rep(i, n) for(int i=0;i<(n);i++)
+#define repi(i, x, n) for(int i=x;i<(n);i++)
+#define all(x) x.begin(), x.end()
+
+struct T{
+    ll l, r;
+};
+
+int main(){
+    int n; cin >> n;
+    vector<T> a(n);
+    rep(i, n){
+        cin >> a[i].l >> a[i].r;
+    }
+    sort(all(a), [](const T &a, const T &b){
+        return a.l < b.l;
+    });
+
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    
+    ll res = 0;
+
+    for (const auto &e : a){
+        while (!pq.empty() && pq.top() < e.l) pq.pop();
+        res += pq.size();
+        pq.push(e.r);
+    }
+    cout << res << endl;
+    return 0;
+}
+
+// ====================SECOND APPROACH===============
 struct T{
     ll pos;
     ll type;
